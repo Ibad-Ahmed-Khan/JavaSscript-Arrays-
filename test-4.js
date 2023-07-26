2264dfb98c11328da0a0da035bb450b01a6912c2
Ibad-Ahmed-Khan <122222523+Ibad-Ahmed-Khan@users.noreply.github.com>
2023-07-26 14:12:05 +0500

    Add files via upload

---
+++ test-4.js
@@ -0,0 +1,10 @@
+//  we provide you with a starting array listing the names of some birds.
+
+// Find the index of the "Eagles" item, and use that to remove the "Eagles" item.
+// Make a new array from this one, called eBirds, that contains only birds from the original array whose names begin with the letter "E". Note that startsWith() is a great way to check whether a string starts with a given character.
+// If it works, you should see "Emus,Egrets" appear in the page.
+
+let birds = ["eagle", "sparrow", "peigon", "hen"];
+for (i = 0; i < birds.length; i++) {
+  console.log(birds[i], [i]);
+}
