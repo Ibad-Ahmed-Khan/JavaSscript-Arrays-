0a18a7d757809ef4f009a8fb95b88dabbf125455
Ibad-Ahmed-Khan <122222523+Ibad-Ahmed-Khan@users.noreply.github.com>
2023-07-28 00:20:45 +0500

    Add files via upload

--- array-table.js
+++ array-table.js
@@ -1,4 +1,6 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (i = 1; i <= arr.length; i = i + 2) {
-  console.log(arr[i]);
+let arr = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+for (let j = 2; j <= arr.length; j = j + 2) {
+  console.log(2, "*", j / 2, "=", j);
 }
