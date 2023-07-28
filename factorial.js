737b2be48a31b8c43d09873fb62f4ef81b1f992f
Lahori Jawan <nasir.khan@tkxel.com>
2023-07-28 18:23:29 +0500

    array exercise

---
+++ factorial.js
@@ -0,0 +1,13 @@
+for (let i = 6; i >= 1; i--) {
+  let fact = i * (i - 1);
+  for (let j = i - 2; j >= 1; j--) {
+    fact = fact * j;
+  }
+  console.log(fact);
+  break;
+}
+
+// 5  * 4 = 20;
+// 20 * 3  = 60;
+// 60 * 2  = 120;
+//120 * 1  = 120;
