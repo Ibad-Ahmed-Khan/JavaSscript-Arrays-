6f56bdbd7ebe6b46cc0f3f58460797310bda06eb
Ibad-Ahmed-Khan <122222523+Ibad-Ahmed-Khan@users.noreply.github.com>
2023-07-28 20:53:18 +0500

    Add files via upload

---
+++ missing.js
@@ -0,0 +1,8 @@
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let sub__array = [1, 3, 5, 6, 8];
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < sub__array.length; j++)
+    if (arr[i] === sub__array[j]) {
+      console.log("union are", arr[i]);
+    }
+}
