e74694e58bbce07b7cbc9b38233f5ebba30d9431
Lahori Jawan <nasir.khan@tkxel.com>
2023-07-30 21:16:00 +0500

    arr

---
+++ rotate.js
@@ -0,0 +1,7 @@
+let arr = [1, 2, 3, 4, 5];
+let item = 0;
+arr.shift();
+arr.shift();
+arr.push(1);
+arr.push(2);
+console.log(arr);
---
+++ swaps.js
@@ -0,0 +1,4 @@
+let arr = [4, 3, 1, 2];
+for (i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
