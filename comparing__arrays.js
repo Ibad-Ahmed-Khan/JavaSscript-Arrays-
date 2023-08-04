da20d62032befbf75acac0c6b044cbaaac48bfa8
Lahori Jawan <nasir.khan@tkxel.com>
2023-08-04 13:13:57 +0500

    comparing__array

--- comparing__arrays.js
+++ comparing__arrays.js
@@ -7,8 +7,10 @@ for (let i = 0; i < arr1.length; i++) {
   if (arr1[i] != arr2[i]) {
     console.log("not same");
     break;
+    // i want a break here if the condition is true.
   }
 }
+
 for (let i = 0; i < 1; i++) {
   if (arr1[i] == arr2[i]) {
     console.log("same");
