8cd04d4e3d7d60df04729bcf734e8137e7bc92f3
Lahori Jawan <nasir.khan@tkxel.com>
2023-08-04 12:59:09 +0500

    comparing__arrays

--- comparing__arrays.js
+++ comparing__arrays.js
@@ -1,17 +1,15 @@
 let arr1 = [51, 39, 59, 78];
-let arr2 = [59, 39, 78, 51];
+let arr2 = [59, 39, 51];
 let arrays__length = arr1.length + arr2.length;
 arr1.sort();
 arr2.sort();
-for (let i = 0; i < arrays__length; i++) {
-  if (arr1.length != arr2.length) {
-    console.log("not same");
-    break;
-  }
+for (let i = 0; i < arr1.length; i++) {
   if (arr1[i] != arr2[i]) {
     console.log("not same");
     break;
   }
+}
+for (let i = 0; i < 1; i++) {
   if (arr1[i] == arr2[i]) {
     console.log("same");
   }
