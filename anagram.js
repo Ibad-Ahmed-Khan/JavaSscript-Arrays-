90c2edd1add3799c7f53d554c4e12b6926e15fcf
Lahori Jawan <nasir.khan@tkxel.com>
2023-08-06 02:43:59 +0500

    anagram

--- anagram.js
+++ anagram.js
@@ -1,1 +1,17 @@
 // tenet -> tenet
+let arr = ["abc", "cba"];
+let string1 = arr[0];
+let string2 = arr[1];
+string1 = string1.split("").sort();
+string2 = string2.split("").sort();
+
+for (let i = 0; i < string2.length; i++) {
+  if (string1[i] !== string2[i]) {
+    console.log("it's not an anagram");
+    break;
+    // i want a powerful break here
+  } else {
+    console.log("yeah it's an anagram");
+    break;
+  }
+}
--- sorting.js
+++ sorting.js
@@ -1,4 +1,4 @@
-let arr = [5, 3, 4, 1, 2, 10, 9, 7, 8, 6];
+arr = [7, 5, , 1, 3, 5, 2, 4, 8, 9, 6, 10];
 for (let i = 0; i < arr.length; i++) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > arr[i + 1]) {
@@ -6,7 +6,7 @@ for (let i = 0; i < arr.length; i++) {
     }
   }
 }
+console.log(arr);
 for (let i = 0; i < arr.length / 2; i++) {
   [arr[i], arr[arr.length - 1 - i]] = [arr[arr.length - 1 - i], arr[i]];
 }
-console.log(arr);
