ae09a4cc3b55ca92525913a7085f1ad16cb2c306
Lahori Jawan <nasir.khan@tkxel.com>
2023-08-08 10:51:15 +0500

    delection counting

--- alternating-string.js
+++ alternating-string.js
@@ -1,7 +1,9 @@
-let arr = ["ABCABCDDEE"];
-let string1 = arr[0];
-for (let i = 0; i < string1.length; i++) {
-  if (string1[i] !== string1[i + 1]) {
-    console.log(string1[i]);
+let str1 = "AAABBB";
+str1 = str1.split("");
+let count = 0;
+for (let i = 0; i < str1.length; i++) {
+  if (str1[i] == str1[i + 1]) {
+    count++;
   }
 }
+console.log(count, "delections");
--- pure-string.js
+++
@@ -1,9 +0,0 @@
-let str1 = "AAABBB";
-str1 = str1.split("");
-let count = 0;
-for (let i = 0; i < str1.length; i++) {
-  if (str1[i] == str1[i + 1]) {
-    count++;
-  }
-}
-console.log(count, "delections");
