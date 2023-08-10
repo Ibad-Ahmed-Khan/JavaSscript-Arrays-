9a05e89385a2f44795e3d01e87b9a308f80bbeca
Lahori Jawan <nasir.khan@tkxel.com>
2023-08-10 17:24:48 +0500

    again

--- counting-characters.js
+++ counting-characters.js
@@ -1,19 +1,14 @@
 // Write a JavaScript program to count the number of characters (character frequency) in a string.
 // Sample String : google.com'
-let str = "google.com".split("");
+let str = "google.com".split("").sort();
 let count = 1;
-for (let i = 0; i < str.length; i++) {
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] > str[i + 1]) {
-      [str[i], str[i + 1]] = [str[i + 1], str[i]];
-    }
-  }
-}
+
 for (let i = 0; i < str.length; i++) {
   if (str[i] == str[i + 1]) {
     count++;
-  } else {
-    let value = [str[i], count].toString();
+  }
+  if (str[i] !== str[i + 1]) {
+    let value = [str[i], count++].toString();
     count = 1;
     console.log(value);
   }
