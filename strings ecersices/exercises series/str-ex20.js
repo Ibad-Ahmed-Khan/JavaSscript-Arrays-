8fee3fd71b1e19dab53b0b4ca6cc4ace817196f6
Lahori Jawan <nasir.khan@tkxel.com>
2023-08-12 10:51:54 +0500

    Description of changes made in str-ex20.js

---
+++ strings ecersices/exercises series/str-ex20.js
@@ -0,0 +1,4 @@
+//  function to reverse a string if its length is a multiple of 4.
+let str = "function to reverse a string".split(" ");
+str = str.reverse().toString().replaceAll(",", " ");
+console.log(str);
