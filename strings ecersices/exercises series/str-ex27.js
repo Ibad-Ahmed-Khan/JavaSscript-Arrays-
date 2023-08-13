2552e0aeda93f51713759e03bb2522aef3bffcdb
Lahori Jawan <nasir.khan@tkxel.com>
2023-08-13 13:11:19 +0500

    Description of changes made in str-ex27.js

--- strings ecersices/exercises series/str-ex27.js
+++ strings ecersices/exercises series/str-ex27.js
@@ -1,5 +1,7 @@
 // program to remove existing indentation from all of the lines in a given text.
 let str =
-  "'''Python is a widely used high-level, general-purpose, interpreted,dynamic programming language. Its design philosophy emphasizescode readability, and its syntax allows programmers to expressconcepts in fewer lines of code than possible in languages suchas C++ or Java.'''";
-str = str.replaceAll("'", "");
-console.log(str);
+  "Python is a widely used high-level, general-purpose. interpreted,dynamic programming language. Its design philosophy emphasizescode readability., and its syntax allows programmers to expressconcepts. in fewer lines of code than possible. in languages suchas C++ or Java.";
+str = str.split(".");
+for (let i = 0; i < str.length - 1; i++) {
+  console.log(">", str[i]);
+}
