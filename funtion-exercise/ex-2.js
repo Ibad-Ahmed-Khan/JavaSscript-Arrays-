863e7f0e060a8c75598d8c79138998b1b0a6ef21
Lahori Jawan <nasir.khan@tkxel.com>
2023-08-14 21:57:51 +0500

    Description of changes made in function-ex1.js

---
+++ funtion-exercise/ex-2.js
@@ -0,0 +1,8 @@
+function percentage(Obtain) {
+  return Math.ceil((Obtain / 1100) * 100) + " " + "%" + " " + "out of 1100";
+}
+
+let NasirBhai = console.log(percentage(1100));
+let ibad = console.log(percentage(756));
+let Shakeela = console.log(percentage(500));
+let Rizwana = console.log(percentage(350));
