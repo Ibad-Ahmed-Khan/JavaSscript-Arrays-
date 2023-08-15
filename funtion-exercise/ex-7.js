c3d6b2874177a8c3ddfc470fa2eeb5ce8a8dcfaa
Lahori Jawan <nasir.khan@tkxel.com>
2023-08-15 22:12:55 +0500

    Description of changes made in ex-7.js

---
+++ funtion-exercise/ex-7.js
@@ -0,0 +1,11 @@
+// function to calculate the resultorial of a number (a non-negative integer).
+//  The function accepts the number as an argument.
+// resultorial of 4 is 24.
+function factorial(n) {
+  let result = n;
+  for (let i = result - 1; i > 1; i--) {
+    result = result * i;
+  }
+  return "factorail is" + " " + n + " " + "is " + result;
+}
+console.log(factorial(5));
