b02cde39cbc01ef11625137bb627325a010afd1f
Lahori Jawan <nasir.khan@tkxel.com>
2023-08-17 16:08:38 +0500

    Description of changes made in 19.js

--- funtion-exercise/ex-9.js
+++ funtion-exercise/ex-9.js
@@ -1,27 +1,17 @@
 // function that takes a number as a parameter and checks whether the number is prime or not.
-function primenumber(n) {
-  if (n % 2 == 0) {
-    if (n == 2) {
-      return n + " " + "is prime number";
+// Updated prime 2.O
+function lcm(n) {
+  n = 11;
+  let count = 0;
+  for (let i = n - 1; i >= 1; i--) {
+    if (n % i == 0) {
+      count += i;
     }
-    return n + " " + "not prime number";
   }
-
-  if (n % 3 == 0) {
-    if (n == 3) {
-      return n + " " + "is prime number";
-    }
-    return n + " " + "not prime number";
-  }
-
-  if (n % 5 == 0) {
-    if (n == 5) {
-      return n + " " + "is prime number";
-    }
-    return n + " " + "not prime number";
+  if (count == 1) {
+    return "its a prime number";
   } else {
-    return n + " " + "is prime number";
+    return "its not a prime number";
   }
 }
-let Result = primenumber(111);
-console.log(Result);
+console.log(lcm());
