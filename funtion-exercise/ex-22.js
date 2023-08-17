d3908230d773243d64dd6b5bbe3dfae3026d7015
Lahori Jawan <nasir.khan@tkxel.com>
2023-08-17 18:38:38 +0500

    Description of changes made in 21.js

---
+++ funtion-exercise/ex-22.js
@@ -0,0 +1,12 @@
+// checking color
+
+let arr = [
+  { Name: "alo", price: 70 },
+  { Name: "gobi", price: 100 },
+  { Name: "gajar", price: 10 },
+  { Name: "moli", price: 20 },
+  { Name: "matar", price: 120 },
+];
+
+let check = arr.filter((arr) => arr.price >= 50);
+console.log(check);
