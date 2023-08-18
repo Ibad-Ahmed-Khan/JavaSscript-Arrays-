547e023598d1aecf86db29dd7eed1e01e9d7ebfb
Lahori Jawan <nasir.khan@tkxel.com>
2023-08-18 15:12:18 +0500

    Description of changes made in ex-26.js

---
+++ funtion-exercise/ex-26.js
@@ -0,0 +1,4 @@
+// Given an array of strings, return a new array that contains the length of each string.
+let arr = ["ibad", "ahmed", "khan", "pahan", "lala", "batan"];
+let string__length = arr.map((arr) => arr.length);
+console.log(string__length);
